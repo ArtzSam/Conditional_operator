66c6f60d18ad267c43ce46315d21bdfe4413f57e
ArtzSam <64463065+ArtzSam@users.noreply.github.com>
2023-10-09 20:51:07 +0300

    Add files via upload
    Задание 1
    Пользователь вводит целое число. Проверьте, является ли это число четным. Если число является четным, выведите «Данное число четное», если наоборот – выведите «Данное число не является четным»
    
    Пример:
    Ввод: 190
    Вывод: Данное число четное
    
    Ввод: 113
    Вывод: Данное число не является четным
    
    Задание 2
    Пользователь вводит 2 числа. 
    Вывести на экран большее из этих чисел
    
    Пример:
    Ввод: 3 4 
    Вывод: 4
    
    Задание 3
    Проверить является ли клетка шахматной доски белой
    Пользователь вводит 2 числа (координаты клетки шахматного поля, от 1 до 8)
     Вывести YES, если клетка белая, и NO, если клетка черная
    
    Пример:
    Ввод: 5 5
    Вывод: NO

---
+++ 2.py
@@ -0,0 +1,7 @@
+num1 = int(input("Введите первое число: "))
+# заместо int - можно использовать float для переменных с запятой
+num2 = int(input("Введите второе число: "))
+if num1 > num2:
+    print(num1)
+else:
+    print(num2)
